decb66ac2bcffb791cbe20ac668ec18f9e3e5e65
Gabriel Franco <gf12166@gmail.com>
2024-08-21 19:34:42 -0300

    primeiro commit

---
+++ src/br/com/minhaempresa/aplicacaodecompras/principal/CartaoDeCredito.java
@@ -0,0 +1,40 @@
+package br.com.minhaempresa.aplicacaodecompras.principal;
+
+import br.com.minhaempresa.aplicacaodecompras.modolos.Compra;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CartaoDeCredito {
+    private double saldo;
+    private double limite;
+    private List<Compra> compras;
+
+    public CartaoDeCredito(double limite) {
+        this.limite = limite;
+        this.saldo = limite;
+        this.compras = new ArrayList<>();
+    }
+
+    public boolean lancaCompra(Compra compra) {
+        if (this.saldo >= compra.getValor()) {
+            this.saldo -= compra.getValor();
+            this.compras.add(compra);
+            return true;
+        }
+        return false;
+
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public double getLimite() {
+        return limite;
+    }
+
+    public List<Compra> getCompras() {
+        return compras;
+    }
+}
